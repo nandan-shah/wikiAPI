4a778fc3a124eef45476a1cdcc54a5afa82af88f
nandan-shah <nandan2583@gmail.com>
2021-06-28 11:54:31 +0530

    articles route done

--- app.js
+++ app.js
@@ -23,10 +23,53 @@ const articleSchema = new mongoose.Schema({
 const Article = mongoose.model('Article',articleSchema);
 //mongoose setup complete
 
-//home route get request
-app.get('/', (req, res) => {
-res.send('Hello');
+//home route
+app.route('/articles')
+.get((req, res) => {
+
+    Article.find({},function(err, foundArticles) {
+        if(!err){
+            res.send(foundArticles);
+        }
+        else{
+            res.send(err);
+        }
+    });
+})
+.post((req,res)=>{
+    const newArticle= new Article({
+    title:req.body.title,
+    content:req.body.content
+    });
+    newArticle.save(function(err){
+        if(!err){
+            res.send("successfully post added");
+        }
+    });
+    })
+.delete((req,res)=>{
+    Article.deleteMany({},function(err){
+        if(!err){
+            res.send("articles deleted");
+        }
+    });
+
 });
+
+
+app.route("/articles/:articleTitle")
+.get(function(req,res){
+    const articleTitle = req.params.articleTitle;
+    Article.findOne({title: articleTitle},function(err,foundArticle){
+        if(foundArticle){
+            res.send(foundArticle);
+        }
+        else{
+            res.send("no article with matching title");
+        }
+    });
+});
+
 
 //server connect
 app.listen('3000', ()=>{
